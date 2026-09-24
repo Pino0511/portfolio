f757bcc031b8071bbe343a47495823d6714629ed
Jacopo Russo <jacopo.russo@linksmt.it>
2026-09-24 15:01:08 +0200

    chore: remove secondary projects from public portfolio

--- creature-api/warofraces/warofraces/src/main/java/dev/russojacopo/warofraces/repository/CreatureRepository.java
+++
@@ -1,9 +0,0 @@
-package dev.russojacopo.warofraces.repository;
-
-import org.springframework.data.repository.CrudRepository;
-
-import dev.russojacopo.warofraces.models.Creature;
-
-public interface CreatureRepository extends CrudRepository<Creature, Long> {
-
-}
